57045fcd670ab2feee3ca084c1d5c68e60309591
Julien TESSIEAU <julien.tessieau@gmail.com>
2021-12-14 20:54:02 +0000

    refactor(model): rewrite jobModel

--- src/Models/JobModel.php
+++ src/Models/JobModel.php
@@ -18,70 +18,49 @@ class JobModel extends AbstractModel
     private int $user_id;
     private int $company_id;
 
-    /**
-     * @return int
-     */
+    public function __construct()
+    {
+        $this->table = "jobs";
+    }
+
     public function getId(): int
     {
         return $this->id;
     }
 
-    /**
-     * @param int $id
-     * @return JobModel
-     */
     public function setId(int $id): JobModel
     {
         $this->id = $id;
         return $this;
     }
 
-    /**
-     * @return string
-     */
     public function getDesignation(): string
     {
         return $this->designation;
     }
 
-    /**
-     * @param string $designation
-     * @return JobModel
-     */
     public function setDesignation(string $designation): JobModel
     {
         $this->designation = $designation;
         return $this;
     }
 
-    /**
-     * @return float
-     */
     public function getRate(): float
     {
         return $this->rate;
     }
 
-    /**
-     * @param int $rate
-     * @return JobModel
-     */
     public function setRate(float $rate): JobModel
     {
         $this->rate = $rate;
         return $this;
     }
 
-
     public function getStartDate(): string
     {
         return $this->startDate;
     }
 
-    /**
-     * @param $startDate
-     * @return JobModel
-     */
     public function setStartDate($startDate): JobModel
     {
         $this->startDate = $startDate;
@@ -93,91 +72,65 @@ class JobModel extends AbstractModel
         return $this->endDate;
     }
 
-    /**
-     * @return JobModel
-     */
     public function setEndDate($endDate = null): JobModel
     {
         $this->endDate = $endDate;
         return $this;
     }
 
-    /**
-     * @return string
-     */
     public function getPeriodOfWork(): string
     {
         return $this->periodOfWork;
     }
 
-    /**
-     * @param string $periodOfWork
-     * @return JobModel
-     */
     public function setPeriodOfWork(string $periodOfWork): JobModel
     {
         $this->periodOfWork = $periodOfWork;
         return $this;
     }
 
-    /**
-     * @return int
-     */
     public function getFirstDayOfTheWeek(): int
     {
         return $this->firstDayOfTheWeek;
     }
 
-    /**
-     * @param int $firstDayOfTheWeek
-     * @return JobModel
-     */
     public function setFirstDayOfTheWeek(int $firstDayOfTheWeek): JobModel
     {
         $this->firstDayOfTheWeek = $firstDayOfTheWeek;
         return $this;
     }
 
-    /**
-     * @return int
-     */
+
     public function getUserId(): int
     {
         return $this->user_id;
     }
 
-    /**
-     * @param int $user_id
-     * @return JobModel
-     */
+
     public function setUserId(int $user_id): JobModel
     {
         $this->user_id = $user_id;
         return $this;
     }
 
-    /**
-     * @return int
-     */
+
     public function getCompanyId(): int
     {
         return $this->company_id;
     }
 
-    /**
-     * @param int $company_id
-     * @return JobModel
-     */
     public function setCompanyId(int $company_id): JobModel
     {
         $this->company_id = $company_id;
         return $this;
     }
 
-    public function createJob()
+    // Database interactio 
+
+    public function create(): ?int
     {
         $sql =
-            'INSERT INTO jobs (
+            "INSERT INTO $this->table (
                 job_designation,
                 job_rate,
                 job_start_date,
@@ -187,7 +140,7 @@ class JobModel extends AbstractModel
                 company_id,
                 user_id
             )
-            VALUES (?,?,?,?,?,?,?,?)';
+            VALUES (?,?,?,?,?,?,?,?)";
 
         $pdo = $this->getPDO();
         $stmt = $pdo->prepare($sql);
@@ -202,40 +155,32 @@ class JobModel extends AbstractModel
             $this->getCompanyId(),
             $this->getUserId(),
         ]);
-        if ($return) {
-            return $pdo->lastInsertId();
-        } else {
-            return false;
-        }
+
+        return $return ? $pdo->lastInsertId() : NULL;
     }
 
-    public function findAllByUserId($id)
+    public function findAllByUserId($id): ?array
     {
         $pdo = $this->getPDO();
-        $stmt = $pdo->prepare(
-            'SELECT jobs.*, companies.company_name
-            FROM jobs
+
+        $sql =
+            "SELECT jobs.*, companies.company_name
+            FROM $this->table AS jobs
             JOIN companies ON jobs.company_id=companies.company_id
-            WHERE user_id=?'
-        );
+            WHERE user_id=?";
+
+        $stmt = $pdo->prepare($sql);
         $stmt->execute([$id]);
 
-        return $stmt->fetchAll();
+        $return = $stmt->fetchAll();
+
+        return $return !== false ? $return : NULL;
     }
 
-    public function findOne(int $id): array
-    {
-        $pdo = $this->getPDO();
-        $stmt = $pdo->prepare('SELECT * FROM jobs WHERE job_id=?');
-        $stmt->execute([$id]);
-
-        return $stmt->fetch();
-    }
-
-    public function updateJob(array $formData)
+    public function update(array $formData)
     {
         $sql =
-            'UPDATE jobs
+            "UPDATE $this->table
             SET
                 job_designation=?,
                 job_rate=?,
@@ -244,7 +189,7 @@ class JobModel extends AbstractModel
                 job_pay_period=?,
                 job_first_day_of_the_week=?,
                 company_id=?
-            WHERE job_id=?';
+            WHERE job_id=?";
 
         $pdo = $this->getPDO();
         $stmt = $pdo->prepare($sql);
@@ -259,10 +204,6 @@ class JobModel extends AbstractModel
             $formData['companyId'],
             $formData['id']
         ]);
-        if ($return) {
-            return $pdo->lastInsertId();
-        } else {
-            return false;
-        }
+        return $return ? $pdo->lastInsertId() : NULL;
     }
 }
