6116e6bfb21eaea5a6ee9821292cc78b983ab577
Julien TESSIEAU <julien.tessieau@gmail.com>
2022-01-31 18:01:44 +0000

    feat(homepage): add redirection to job list if connected

--- src/Controllers/Home/HomeController.php
+++ src/Controllers/Home/HomeController.php
@@ -3,14 +3,20 @@
 namespace App\Controllers\Home;
 
 use App\Controllers\Utils\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class HomeController extends AbstractController
 {
     public function index()
     {
+        if (isset($_SESSION['user'])) {
+            $res = new RedirectResponse('/job/list');
+            $res->send();
+        }
+
         return $this->render('home', [
             "title" => "Wtt - Homepage",
-            "hideNavabar" => true
+            "hideNavbar" => true
         ]);
     }
 }
