924ba2001419871b1bc172fce17b0493de499950
Julien TESSIEAU <julien.tessieau@gmail.com>
2022-02-01 19:18:08 +0000

    style(homepage): add horizontal spacing to the page

--- src/Views/home.php
+++ src/Views/home.php
@@ -1,30 +1,32 @@
    <div class="hero is-fullheight">
       <div class="hero-body">
-         <div class="columns is-vcentered">
-            <div class="column p-4">
-               <div class="hero-text">
-                  <p class="title is-1">
-                     Working Time Tracker
-                  </p>
-                  <p class="subtitle is-2">
-                     One place to store all your working time!
-                  </p>
-                  <p class="is-size-4 mb-6">
-                     Store your check-in/check-out, break time, pay rate ...
-                     And never forget how many hours you worked last month or last year.
-                  </p>
-                  <div class="buttons">
-                     <a href="/signin" class="button is-primary">
-                        <strong>Sign in</strong>
-                     </a>
-                     <a href="login" class="button is-light">
-                        Log in
-                     </a>
+         <div class="container">
+            <div class="columns is-vcentered">
+               <div class="column p-4">
+                  <div class="hero-text">
+                     <p class="title is-1">
+                        Working Time Tracker
+                     </p>
+                     <p class="subtitle is-2">
+                        One place to store all your working time!
+                     </p>
+                     <p class="is-size-4 mb-6">
+                        Store your check-in/check-out, break time, pay rate ...
+                        And never forget how many hours you worked last month or last year.
+                     </p>
+                     <div class="buttons">
+                        <a href="/signin" class="button is-primary">
+                           <strong>Sign in</strong>
+                        </a>
+                        <a href="login" class="button is-light">
+                           Log in
+                        </a>
+                     </div>
                   </div>
                </div>
-            </div>
-            <div class=" column is-hidden-touch	p-4">
-               <img src="images/undraw_quitting_time_dm-8-t.svg" alt="">
+               <div class=" column is-hidden-touch	p-4">
+                  <img src="images/undraw_quitting_time_dm-8-t.svg" alt="">
+               </div>
             </div>
          </div>
       </div>
