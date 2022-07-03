5353f3449ed363c1793191562e0cc400dcdd2f33
Julien TESSIEAU <julien.tessieau@gmail.com>
2022-07-03 03:27:54 +0100

    [feat]  list job as card instead of using a table
    - add a style.css files to customize positionning and hover effect. No more 100% using the framework.

--- src/Views/job/jobList.php
+++ src/Views/job/jobList.php
@@ -1,62 +1,58 @@
 <div class="section">
-    <div class="container">
+    <div class="container job-list">
         <?php if (!empty($jobs)) { ?>
-            <h1 class="title has-text-centered">List of your jobs</h1>
-            <table class="table mx-auto">
-                <thead>
-                    <td class="has-text-centered">Designation</td>
-                    <td class="has-text-centered">Company</td>
-                    <td class="has-text-centered">Rate</td>
-                    <td class="has-text-centered">Start date</td>
-                    <td class="has-text-centered">Status</td>
-                    <td class="has-text-centered">Edit</td>
-                    <td class="has-text-centered">Delete</td>
-                </thead>
-                <tbody>
+            <h1 class="title has-text-centered">Your Registred Jobs</h1>
+            <div class="columns is-multiline">
+                <?php foreach ($jobs as $job) :
+                    $startDate = new DateTime($job['job_start_date']);
 
-                    <?php foreach ($jobs as $job) :
-                        $startDate = new DateTime($job['job_start_date']);
+                    if ($job['job_end_date'] == null) {
+                        $active = true;
+                    } else {
+                        $active = false;
+                    }
+                ?>
+                    <div class="column is-4">
+                        <div class="card">
+                            <a href="/job/checkin/list/<?= $job['id'] ?>">
+                                <div class="card-content">
+                                    <p class="title is-4">
+                                        <?= ucwords($job['job_designation']) ?>
+                                    </p>
+                                    <p class="subtitle is-6">
+                                        <?= ucfirst($job['company_name']) ?>
+                                    </p>
+                                </div>
+                            </a>
 
-                        if ($job['job_end_date'] == null) {
-                            $active = true;
-                        } else {
-                            $active = false;
-                        }
-                    ?>
-
-                        <tr>
-                            <td><a href="/job/checkin/list/<?= $job['id'] ?>"><?= ucwords($job['job_designation']) ?></a></td>
-                            <td><?= ucfirst($job['company_name']) ?></td>
-                            <td class="has-text-centered"> £<?= $job['job_rate'] ?></td>
-                            <td class="has-text-centered"><?= $startDate->format('d/m/Y'); ?></td>
-                            <td class="has-text-centered">
+                            <div class="is-active">
                                 <?=
                                 $active ?
-                                    '<i class="fas fa-circle has-text-success"></i>'
+                                    '<i class="fas fa-circle is-size-5 has-text-success"></i>'
                                     :
-                                    '<i class="fas fa-circle has-text-danger"></i>';
+                                    'i class="fas fa-circle has-text-danger"></i>';
                                 ?>
-                            </td>
-                            <td class="has-text-centered">
-                                <a href="/job/update/<?= $job['id'] ?>">
-                                    <i class="far fa-edit has-text-info"></i>
-                                </a>
-                            </td>
-                            <td class="has-text-centered">
-                                <a href="/job/delete/<?= $job['id'] ?>">
-                                    <i class="far fa-trash-alt has-text-danger"></i>
-                                </a>
-                            </td>
-                        </tr>
+                            </div>
 
-                    <?php endforeach ?>
-
-                </tbody>
-            </table>
-        <?php } else {
+                            <footer class="card-footer">
+                                <a href="/job/update/<?= $job['id'] ?>" class="card-footer-item edit-job">
+                                    <i class=" far fa-edit mr-2"></i>Edit
+                                </a>
+                                <a href=" /job/delete/<?= $job['id'] ?>" class="card-footer-item delete-job">
+                                    <i class="far fa-trash-alt mr-2"></i>Delete
+                                </a>
+                            </footer>
+                        </div>
+                    </div>
+                <?php endforeach ?>
+            </div>
+        <?php
+        } else {
             echo '<h1 class="title has-text-centered">You have no jobs registred yet</h1>';
-        } ?>
-        <div class="has-text-centered">
+        }
+        ?>
+
+        <div class=" has-text-centered mt-4">
             <a class="button is-primary" href="/job/start">Register a new job</a>
         </div>
     </div>
