6b474853873f9803a00e44cf78a1932cc7eb33e0
Julien TESSIEAU <julien.tessieau@gmail.com>
2022-07-11 21:15:00 +0100

    [feat] add action button to edit, delete or checkin

--- src/Views/job/jobDetails.php
+++ src/Views/job/jobDetails.php
@@ -2,7 +2,19 @@
     <h1 class="title"><?= $job['job_designation']; ?></h1>
     <p class="subtitle"><?= ucfirst($job['company_name']); ?></p>
 
-    <h2>Last 10 check-ins</h2>
+    <a href="/job/update/<?= $job['id']; ?>" class="button is-info">
+        <i class="far fa-edit mr-2"></i>Edit
+    </a>
+    <a href="/job/delete/<?= $job['id']; ?>" class="button is-danger">
+        <i class="far fa-trash-alt mr-2"></i>Delete
+    </a>
+    <a href="/job/checkin/<?= $job['id']; ?>" class="button is-primary">
+        <i class="far fa-calendar-plus mr-2"></i>Checkin
+    </a>
+</div>
+
+<div class="section">
+    <h2 class="title is-4">Last 10 check-ins</h2>
     <?php if (count($checkins) > 0) : ?>
         <ul>
             <?php foreach ($checkins as $checkin) : ?>
